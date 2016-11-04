0efa9283892e856fd7a3f88a151caf6cafdf36a7
Drmanhattan <92lucifermorningstar@gmail.com>
2016-11-04 15:08:49 +0530

    v4 with indentation changes

--- LoCoCiParser.java
+++ LoCoCiParser.java
@@ -21,98 +21,98 @@ import java.util.*;
  */
 class LoCoCiParser{
 
- //List<String> output ;
+	//List<String> output ;
 
 	/* Default constructor */
 	public LoCoCiParser(){	
 	}
-	
-/**
+
+	/**
 	 * Method for Scraping the path.Splitting takes place here.
 	 * @param path
 	 * @return HashMap evaluated from the respective functions called during this methods function.
 	 * 
 	 */
- public HashMap<String,String> scraping_path(String path){
-	 
+	public HashMap<String,String> scraping_path(String path){
 
- String[] distinct = path.split("/");
- String com = distinct[distinct.length -1];
- String[] component = com.split("-");
-  if(component[0].equals("co")){
-	   return country_type(component);
-	   }
-  
- else if(component[0].equals("ci")){
-	 
-    return city_type(component);
- }
-  
-  else if(component[0].equals("lo")){
-	   
-    return local_type(component);
-  }
-  else
-   return null;
 
-}
+		String[] distinct = path.split("/");
+		String com = distinct[distinct.length -1];
+		String[] component = com.split("-");
+		if(component[0].equals("co")){
+			return country_type(component);
+		}
 
- /**
-  * Method for the country_type
-  * @param component
-  * @return a HashMap containing two key-value pairs
-  * 
-  */
- 
- public HashMap<String,String> country_type(String[] component){
-   String country_id = component[1];
-   String surgery_id = component[2];
- 
-   /* co_su is the map containing the country and surgery ids */
-   HashMap<String,String> co_su = new HashMap<String,String>();
-   co_su.put("Country_Id", country_id);
-   co_su.put("Surgery_Id", surgery_id);
-   
-   return co_su;
-}
+		else if(component[0].equals("ci")){
 
- /**
-  * Method for city_type
-  * @param component
-  * @return Hashmap containing two key-value pairs
-  */
-public HashMap<String,String> city_type(String[] component){
-   String city_id = component[1];
-   String surgery_id = component[2];
- 
- /* ci_su is the map containing the city and surgery ids */
-   HashMap<String,String> ci_su = new HashMap<String,String>();
-   ci_su.put("City_Id",city_id);
-   ci_su.put("Surgery_Id", surgery_id);
-   
-   return ci_su;
-   
-}
+			return city_type(component);
+		}
 
-/**
- * Method for localtype
- * @param component
- * @return a HashMap containing three key-value pairs
- */
-public HashMap<String,String> local_type(String[] component){
-   String locality_id= component[1];
-   String city_id = component[2];
-   String country_id = component[component.length -1];
- 
- /*lococi is the map containing the locality, city, country ids */
-   HashMap<String,String> lococi = new HashMap<String,String>();
-   lococi.put("Locality_Id",locality_id);
-   lococi.put("City_Id", city_id);
-   lococi.put("Country_Id", country_id);
-   
-   return lococi;
-   
-}
+		else if(component[0].equals("lo")){
+
+			return local_type(component);
+		}
+		else
+			return null;
+
+	}
+
+	/**
+	 * Method for the country_type
+	 * @param component
+	 * @return a HashMap containing two key-value pairs
+	 * 
+	 */
+
+	public HashMap<String,String> country_type(String[] component){
+		String country_id = component[1];
+		String surgery_id = component[2];
+
+		/* co_su is the map containing the country and surgery ids */
+		HashMap<String,String> co_su = new HashMap<String,String>();
+		co_su.put("Country_Id", country_id);
+		co_su.put("Surgery_Id", surgery_id);
+
+		return co_su;
+	}
+
+	/**
+	 * Method for city_type
+	 * @param component
+	 * @return Hashmap containing two key-value pairs
+	 */
+	public HashMap<String,String> city_type(String[] component){
+		String city_id = component[1];
+		String surgery_id = component[2];
+
+		/* ci_su is the map containing the city and surgery ids */
+		HashMap<String,String> ci_su = new HashMap<String,String>();
+		ci_su.put("City_Id",city_id);
+		ci_su.put("Surgery_Id", surgery_id);
+
+		return ci_su;
+
+	}
+
+	/**
+	 * Method for localtype
+	 * @param component
+	 * @return a HashMap containing three key-value pairs
+	 */
+	public HashMap<String,String> local_type(String[] component){
+		String locality_id= component[1];
+		String city_id = component[2];
+		String country_id = component[component.length -1];
+
+		/*lococi is the map containing the locality, city, country ids */
+		HashMap<String,String> lococi = new HashMap<String,String>();
+		lococi.put("Locality_Id",locality_id);
+		lococi.put("City_Id", city_id);
+		lococi.put("Country_Id", country_id);
+
+		return lococi;
+
+	}
 
 }
 
--- ParsingUrl.java
+++ ParsingUrl.java
@@ -4,28 +4,28 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class ParsingUrl extends LoCoCiParser {
-	
+
 	public ParsingUrl(){
-		
+
 	}
-	
-	 public static void main(String[] args) throws Exception{
-     Scanner input = new Scanner(System.in);
-     System.out.println("Enter the URL to parse??");
-	 String uri = input.nextLine();
-	 URL url = new URL(uri);	
-	 String path = url.getPath();
-	 
-	 LoCoCiParser scraper = new LoCoCiParser();
-	 
-	 //returns a hashed object
-	 //scrapingpath should return a hashed object 
-	 
-	 HashMap<String,String> map = new HashMap<String,String>();
-	 
-	 map = scraper.scraping_path(path);
-	 
-		 System.out.println(map);
-	 }
+
+	public static void main(String[] args) throws Exception{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the URL to parse??");
+		String uri = input.nextLine();
+		URL url = new URL(uri);	
+		String path = url.getPath();
+
+		LoCoCiParser scraper = new LoCoCiParser();
+
+		//returns a hashed object
+		//scrapingpath should return a hashed object 
+
+		HashMap<String,String> map = new HashMap<String,String>();
+
+		map = scraper.scraping_path(path);
+
+		System.out.println(map);
+	}
 
 }
